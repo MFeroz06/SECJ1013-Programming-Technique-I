6075cd4701526d3fd5a177fd1d0ac0801e0daa94
Muhammad Fairuz Bin Herman <mfairuz06@gmail.com>
2025-12-28 01:24:24 +0800

    Add files via upload

---
+++ Practice/practice2312.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+/*cout << "*" << -17 << "*" << endl;
+cout << "*" << setw(6) << -17 << "*" << endl << endl;
+cout << "*" << "Hi there!" << "*" << endl;
+cout << "*" << setw(20) << "Hi there!" << "*" << endl;
+cout << "*" << setw(3) << "Hi there!" << "*" << endl;
+
+int x = 15;
+int y = 7634;
+cout << left;
+cout << setw(5) << x << setw(7) << y << setw(8) << "Warm"
+<< endl;
+cout << right;
+cout << setw(5) << x << setw(7) << y << setw(8) << "Warm"
+<< endl;
+
+cout << "*" << -17 << "*" << endl;
+cout << "*" << setw(6) << -17 << "*" << endl;
+cout << left;
+cout << "*" << setw(6) << -17 << "*" << endl << endl;
+cout << "*" << "Hi there!" << "*" << endl;
+cout << "*" << setw(20) << "Hi there!" << "*" << endl;
+cout << right;
+cout << "*" << setw(20) << "Hi there!" << "*" << endl;
+
+double x = 15.674;
+double y = 235.73;
+double z = 9525.9874;
+cout << cout<< showpoint;
+cout << x << endl << y << endl << z << endl;
+
+double x = 15.674;
+double y = 235.73;
+double z = 9525.9874;
+cout << setprecision(2);
+cout << x << endl << y << endl << z << endl;
+
+const int SIZE = 10;
+char firstName[SIZE];
+cout << "Enter your name: ";
+cin >> setw(SIZE) >> firstName;
+cout <<firstName;*/
+
+char ch;
+cout << "Strike any key to continue";
+cin.get(ch);
+
+return 0;
+}
